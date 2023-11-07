ee43ab945ad41b0a44706eb6f2201ab83e6b6fa1
Yakub <yakubyakubov2003@mail.ru>
2023-11-07 16:34:53 +0100

    rect can move

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <QApplication>
-#include <QGraphicsRectItem>
+#include "MyRect.h"
 #include <QGraphicsScene>
 #include <QGraphicsView>
 
@@ -11,11 +11,15 @@ int main(int argc, char *argv[])
 
     QGraphicsScene *scene= new QGraphicsScene();
 
-    QGraphicsRectItem *rect=new QGraphicsRectItem();
+    MyRect *rect=new MyRect();
     rect->setRect(0,0,100,100);
 
     //add item to the scene
     scene->addItem(rect);
+
+    //make an item focusable
+    rect->setFlag(QGraphicsItem::ItemIsFocusable);
+    rect->setFocus();
 
     //add a view
     QGraphicsView *view=new QGraphicsView(scene);
